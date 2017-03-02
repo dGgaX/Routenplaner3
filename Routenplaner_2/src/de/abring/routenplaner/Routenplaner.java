40c547b3e6e1acbceef6dc412c25a4ac2d422f7b
Andreas <Karima@192.168.0.11>
2017-03-02 22:45:31 +0100

    small changes to Main.class

--- Routenplaner_2/src/de/abring/routenplaner/Routenplaner.java
+++ Routenplaner_2/src/de/abring/routenplaner/Routenplaner.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.ConsoleHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
     
